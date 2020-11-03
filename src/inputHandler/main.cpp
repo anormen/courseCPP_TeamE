eca270e0b9944d41c4fdf41d14753b2514c22e21
jsuld <jakob.suld@volvocars.com>
2020-11-03 16:13:19 +0100

    cleaned up X11 stuff

--- src/inputHandler/main.cpp
+++ src/inputHandler/main.cpp
@@ -1,53 +1,6 @@
 #include <iostream>
-#include <X11/Xlib.h>
 #include "socketcan_cpp.h"
-
-
-uint ReadInputKey(Display *display, XEvent &event){
-        
-        uint key = 0;
-        XNextEvent(display, &event);
- 
-        /* keyboard events */
-        if (event.type == KeyPress)
-        {
-            printf( "KeyPress: %x\n", event.xkey.keycode );
-            key = event.xkey.keycode;
-
-            /* exit on ESC key press */
-            //if ( event.xkey.keycode == 0x09 )
-            //    break;
-        }
-        else if (event.type == KeyRelease)
-        {
-            //printf( "KeyRelease: %x\n", event.xkey.keycode );
-        }
-    return key;
-
-}
-
-void SetupKeyInput(Display *display, Window &window){
-
-    int s;
-    //display = XOpenDisplay(NULL);
-    if (display == NULL)
-    {
-        fprintf(stderr, "Cannot open display\n");
-        exit(1);
-    }
- 
-    s = DefaultScreen(display);
- 
-    /* create window */
-    window = XCreateSimpleWindow(display, RootWindow(display, s), 1, 1, 2, 2, 1,
-                           BlackPixel(display, s), WhitePixel(display, s));
- 
-    /* select kind of events we are interested in */
-    XSelectInput(display, window, KeyPressMask | KeyReleaseMask );
- 
-    /* map (show) the window */
-    XMapWindow(display, window);
-}
+#include "key_input_X11.hpp"
 
 int main() {
 
@@ -58,15 +11,12 @@ int main() {
     XEvent event;
     uint key;
     display = XOpenDisplay(NULL);
+
     SetupKeyInput(display,window);
     
-
-
     while(1){
         key = ReadInputKey(display,event);
         std::cout << "key = " << key << std::endl;
-
-    
     }
 
     XCloseDisplay(display);
@@ -87,12 +37,7 @@ int main() {
         sleep
     }
 
-    */
-    
-   /*
-
     clear memory
     */
 
-    //First version with pseudo code
 }
