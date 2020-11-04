8e3aea1517b753cfcfc2f55fd1da238931957519
Mikko Härmä <mikko.harma@volvocars.com>
2020-11-04 13:31:16 +0100

    updated displayhandler

--- src/displayHandler/main.cpp
+++ src/displayHandler/main.cpp
@@ -18,28 +18,38 @@ int main()
     canHandler canhndl;
 
     canhndl.canInit();
-
     can_frame* canRxptr = canhndl.getRxBuffer();
 
     while(1){
         canhndl.canReadFrame();
-        std::cout << "Data: [" << std::setw(2) << std::setfill('0') << (int)canRxptr->can_id << " " << std::setw(2) << std::setfill('0') << (int)canRxptr->can_dlc << " ";
-        for(int i = 0 ; i < 8 ; i++ )
-        {
+        
+        //debug
+        std::cout << "Data: [" << std::setw(3) << std::setfill('0') << (int)canRxptr->can_id << " " << std::setw(2) << std::setfill('0') << (int)canRxptr->can_dlc << " ";
+        for(int i = 0 ; i < 8 ; i++ )            
             std::cout << std::setw(2) << std::setfill('0') << std::hex<< (int)canRxptr->data[i] << (i < 7 ? ":" : "");
-        }
         std::cout << "]" << std::endl;
 
-        if(canRxptr->can_id == 0x64 && canRxptr->can_dlc > 0)
+        //check frame id, check lenght
+        if(canRxptr->can_dlc > 0)
         {
-            std::cout << "decoded CAN data: FR100 Accepedal" << canRxptr->data[0] << std::endl;
+            switch(canRxptr->can_id)
+            {       
+                case 100: //inpur handler
+                    std::cout << "decoded CAN data: FR100 Accelpedal: " << (int)canRxptr->data[0] << std::endl;
+                    break;
+                case 200: //ecm
+                    std::cout << "decoded CAN data: FR200 RPM: " << (int)canRxptr->data[0] << std::endl;
+                    break;
+                case 300: //tcm
+                    break;
+                default:
+                    std::cout << "Invalid can frame id" << std::endl;
+            }
         }
         else
-        {
-            std::cout << "decoded CAN data: FR100 is empty" << std::endl;
-        }
+            std::cout << "can data read buffer is empty" << std::endl;
+  
         
-
         std::this_thread::sleep_for(std::chrono::milliseconds(canhndl.cycletime)); //see if take value from can frames?
     }
     return 0;
