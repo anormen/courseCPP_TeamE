0c1cfe213ac5d9e1dc5893cef309fdf1b1fae24d
Mikko Härmä <mikko.harma@volvocars.com>
2020-11-04 10:32:26 +0100

    can class added

--- share/can_class.h
+++ share/can_class.h
@@ -1,10 +1,21 @@
 #ifndef _CAN_CLASS_H_
 #define _CAN_CLASS_H_ 
 
-int canInit(void);
-int canReadFrame(const int, can_frame&);
-int canWriteFrame(const int, can_frame&);
-const char *ifname = "vcan0";
-const int cycletime = 250; //cycletime for can send
+class canHandler{
+
+    public:
+        canHandler(){};
+        void canInit(void);
+        uint16_t canReadFrame();
+        uint16_t canWriteFrame();
+        ~canHandler() {};
+        const int cycletime = 250; //cycletime for can send
+    private:
+        struct can_frame frameRx;
+        struct can_frame frameTx;        
+        uint16_t canSocket;
+        const char *ifname = "vcan0";
+
+};
 
 #endif
