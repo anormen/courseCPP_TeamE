ad21a0ef89799c3f1822be725d8169456b67efd8
Mikko Härmä <mikko.harma@volvocars.com>
2020-11-06 09:52:32 +0100

    updaet more functions

--- src/displayHandler/display_class.h
+++ src/displayHandler/display_class.h
@@ -1,5 +1,7 @@
 #ifndef _DISPLAY_CLASS_H_
 #define _DISPLAY_CLASS_H_ 
+#include "frames.hpp"
+
 class displayHandler{
 
     public:
@@ -9,15 +11,33 @@ class displayHandler{
         void setValueAcc(int _acc){ accelerator = _acc; };
         void setValueRpm(int _rpm){  rpm = _rpm; };
         void setValueBrake(int _brake){  brake = _brake; };        
-        void setValueMode(int _mode){  mode = _mode; }; 
-        void setValueStartStop(int _start){  startstop = _start; };                 
+        void setValueMode(SimulationMode _mode){  mode = _mode; }; 
+        void setValueStartStop(StartButtonSts _start){  startstop = _start; };      
+        void setValueGearLever(GearLeverPos _gearlever){ gearlever = _gearlever; };
+        void setValueDriverInfo(DriverInformation _driverinfo){  driverinfo = _driverinfo; };
+        void setValueGearActual(int _gearactual){  gearactual = _gearactual; };        
+        void setValueVehicleSpeed(float _vehiclespeed){  vehiclespeed = _vehiclespeed; }; 
+        void setValueFuelConsumption(float _fuelconsumption){  fuelconsumption = _fuelconsumption; }; 
+        void setValueTelltale(float _telltale){  telltale = _telltale; };         
+        void setValueInputUB(bool _inputUb){ inputUb = _inputUb; };
+        void setValueECMUB(bool _ecmUb){  ecmUb = _ecmUb; };
+        void setValueTCMUB(bool _tcmUb){  tcmUb = _tcmUb; };                               
         ~displayHandler(){};
     private:
         int accelerator = 0;
         int rpm = 0;
         int brake = 0;    
-        int mode = 0;
-        int startstop = 0;             
+        SimulationMode mode = SimulationMode::SLEEP;
+        StartButtonSts startstop = StartButtonSts::UNPRESSED;         
+        GearLeverPos gearlever = GearLeverPos::PARK; 
+        DriverInformation driverinfo = DriverInformation::NO_MSG;
+        int gearactual = 0;     
+        float vehiclespeed = 0;
+        float fuelconsumption = 0; 
+        int telltale = 0;
+        bool inputUb = 0;     
+        bool ecmUb = 0;
+        bool tcmUb = 0;         
 };
 
 #endif
