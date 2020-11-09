d31dca5b55d1da0e5753a16322e38836e7c8a894
jsuld <jakobsuld42@gmail.com>
2020-11-09 15:04:37 +0100

    not use enum to convert key-req

--- src/inputHandler/key_converter.cpp
+++ src/inputHandler/key_converter.cpp
@@ -15,7 +15,26 @@ keyConverter::keyConverter()
 UserReq keyConverter::readInputReq()
 {
     this->key = getch();
-    this->required_action = static_cast<UserReq>(key);
+    //this->required_action = static_cast<UserReq>(key);
+
+    // Maybe define map somewhere
+    //std::map<int,UserReq> keyconv_map;
+    //keyconv_map.insert(std::pair<int,UserReq>(259,UserReq::ACC_PED_UP));
+    //keyconv_map.insert(std::pair<int,UserReq>(258,UserReq::ACC_PED_DOWN));
+
+
+    // Should perhaps define key->req pairs/conversion somewhere else, not hardcode i .cpp file
+    if(key==259) // arrow up
+        required_action=UserReq::ACC_PED_UP;
+    else if (key==258) // arrow down
+        required_action=UserReq::ACC_PED_DOWN;
+    else if (key==115) // s
+        required_action=UserReq::STARTBUTTON;
+    else if (key==109) // m
+        required_action=UserReq::SIMULATION_MODE;
+    else
+        required_action=UserReq::UNDEFINED;
     
+
     return required_action;
 }
