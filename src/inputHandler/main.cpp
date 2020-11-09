400660a2613ff98dcc54b30289bdb814be99405c
jsuld <jakob.suld@volvocars.com>
2020-11-09 12:36:25 +0100

    main clean up

--- src/inputHandler/main.cpp
+++ src/inputHandler/main.cpp
@@ -9,16 +9,6 @@
 
 int main()
 {
-    std::cout << "inputHandler\n";
-    // Init key input
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
-
-    int key;
-    nodelay(stdscr, TRUE);
-
     can_frame frame;
     canHandler can;
     can.canInit();
@@ -29,17 +19,12 @@ int main()
 
     while (1)
     {
-        if ((key = getch()) == ERR)
-        {
-            // Do nothing
-        } else {
-            //std::cout << "Input is " << key << "\n\r";
-            userReq = keyConv.readInputReq();
-            conv.fillFrame(frame, userReq);
-            uint16_t b = can.canWriteFrame(frame);
-            if(b!=16)
-                std::cout << "ERROR sending can frame\n\r";
-        }
+        userReq = keyConv.readInputReq();
+        conv.fillFrame(frame, userReq);
+        uint16_t b = can.canWriteFrame(frame);
+        if(b!=16)
+            std::cout << "ERROR sending can frame\n\r";
+
         std::this_thread::sleep_for(std::chrono::milliseconds(fr100_updateRate));
     }
 
