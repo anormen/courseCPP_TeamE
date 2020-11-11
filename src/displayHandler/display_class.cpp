e94542e2efa3e15f3e64d5be12053c2f07acdf81
Mikko Härmä <mikko.harma@volvocars.com>
2020-11-11 13:36:06 +0100

    Updated with string value

--- src/displayHandler/display_class.cpp
+++ src/displayHandler/display_class.cpp
@@ -10,17 +10,18 @@ void display::initDisplay()
 
 void display::update()
 { 
-    std::cout << std::dec << std::setw(3) << std::setfill('0') << "Accelerator:     " << (int)accelerator << " %" << std::endl;
-    std::cout << std::dec << std::setw(3) << std::setfill('0') << "Brake:           " << (int)brake << " %" << std::endl;
-    std::cout << std::dec << std::setw(4) << std::setfill('0') << "EngineSpeed:     " << (int)rpm << " rpm" << std::endl;    
-    std::cout << std::dec << std::setw(2) << std::setfill('0') << "Mode:            " << (int)mode << " " << std::endl; 
-    std::cout << std::dec << std::setw(1) << std::setfill('0') << "StartStop:       " << (int)startstop << " " << std::endl; 
-    std::cout << std::dec << std::setw(1) << std::setfill('0') << "GearLever:       " << (int)gearlever << " " << std::endl; 
-    std::cout << std::dec << std::setw(1) << std::setfill('0') << "GearActual:      " << (int)gearactual << " " << std::endl;  
-    std::cout << std::dec << std::setw(4) << std::setfill('0') << "VehicleSpeed:    " << (int)vehiclespeed << " km/h " << std::endl; 
-    std::cout << std::dec << std::setw(2) << std::setfill('0') << "Telltale:        " << (int)telltale << " " << std::endl;     
-    std::cout << std::dec << std::setw(4) << std::setfill('0') << "Avg Fuel con:    " << (int)fuelconsumption << " l/100km " << std::endl;    
-    std::cout << std::dec << std::setw(1) << std::setfill('0') << "Input ub:        " << (int)inputUb << " " << std::endl; 
-    std::cout << std::dec << std::setw(1) << std::setfill('0') << "ECM ub:          " << (int)ecmUb << " " << std::endl;     
-    std::cout << std::dec << std::setw(1) << std::setfill('0') << "TCM ub:          " << (int)tcmUb << " " << std::endl; 
+    std::cout << std::dec << std::setw(3) << std::setfill('0') << "Accelerator:     " << (int)accelerator   << " %" << std::endl;
+    std::cout << std::dec << std::setw(3) << std::setfill('0') << "Brake:           " << (int)brake         << " %" << std::endl;
+    std::cout << std::dec << std::setw(4) << std::setfill('0') << "EngineSpeed:     " << (int)rpm           << " rpm" << std::endl;    
+    std::cout << std::dec << std::setw(2) << std::setfill('0') << "Mode:            " << modes.at((uint8_t)mode) << "" << std::endl; 
+    std::cout << std::dec << std::setw(1) << std::setfill('0') << "StartStop:       " << (int)startstop     << " " << std::endl; 
+    std::cout << std::dec << std::setw(1) << std::setfill('0') << "GearLever:       " << gears.at((uint8_t)gearlever) << "" << std::endl; 
+    std::cout << std::dec << std::setw(1) << std::setfill('0') << "GearActual:      " << (int)gearactual    << "" << std::endl;  
+    std::cout << std::dec << std::setw(4) << std::setfill('0') << "VehicleSpeed:    " << (int)vehiclespeed  << " km/h" << std::endl; 
+    std::cout << std::dec << std::setw(4) << std::setfill('0') << "DriverInfo msg   " << messages.at((uint8_t)driverinfo) << "" << std::endl;     
+    std::cout << std::dec << std::setw(2) << std::setfill('0') << "Telltale:        " << (int)telltale      << "" << std::endl;     
+    std::cout << std::dec << std::setw(4) << std::setfill('0') << "Avg Fuel con:    " << (int)fuelconsumption << " l/100km" << std::endl;    
+    std::cout << std::dec << std::setw(1) << std::setfill('0') << "Input UB:        " << (int)inputUb       << " " << std::endl; 
+    std::cout << std::dec << std::setw(1) << std::setfill('0') << "ECM UB:          " << (int)ecmUb         << " " << std::endl;     
+    std::cout << std::dec << std::setw(1) << std::setfill('0') << "TCM UB:          " << (int)tcmUb         << " " << std::endl; 
 }
