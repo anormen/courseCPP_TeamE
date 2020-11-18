32419749a7fc94087e555bc44704d49b8c186327
jsuld <jakobsuld42@gmail.com>
2020-11-18 10:09:05 +0100

    added example test and fancy cmakelist

---
+++ test/test_test.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include "frames.hpp"
+#include <iostream>
+#include <vector>
+
+
+class FrameFixture : public ::testing::Test {
+  protected:
+    // NOLINTNEXTLINE
+    frame_100 frm_100;
+    
+
+    void SetUp() override
+    {
+        //frm_100.set_accelerator(20);
+    }
+};
+
+TEST_F(FrameFixture, test_acc)
+{
+    uint8_t acc = 20;
+    frm_100.set_accelerator(acc);
+    EXPECT_EQ(frm_100.get_accelerator(),acc);
+};
+
+TEST_F(FrameFixture, test_acc_fail)
+{
+    uint8_t acc = 20;
+    frm_100.set_accelerator(acc);
+    EXPECT_NE(frm_100.get_accelerator(),10);
+};
