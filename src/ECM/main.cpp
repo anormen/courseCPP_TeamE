020ad4736419697d7270ba8bb0925d0719ad5dcd
jsuld <jakobsuld42@gmail.com>
2020-11-18 08:14:51 +0100

    test

--- src/ECM/main.cpp
+++ src/ECM/main.cpp
@@ -17,7 +17,7 @@ int main()
     can.canInit("vcan0");
     frame_100 data_100;
     frame_200 data_200;
-    frame_300 data_300;    
+    frame_300 data_300;
     can_frame frame;
 
     ECM ecm;
@@ -30,16 +30,16 @@ int main()
             std::cout << "read frame\n";
             can.canReadFrame(frame);
             can.printFrame(frame);
-            
+
             if (frame.can_dlc > 0)
             {
-                    std::lock_guard<std::mutex> guard(data_100.fr100_mutex);
-                    if (frame.can_id = 100)
-                        memcpy(data_100.get_frame_ptr(), &frame, sizeof(frame));                        
-                    else if (frame.can_id = 300)
-                        memcpy(data_300.get_frame_ptr(), &frame, sizeof(frame));
-                    //else
-                        //do nothing
+                std::lock_guard<std::mutex> guard(data_100.fr100_mutex);
+                if (frame.can_id = 100)
+                    memcpy(data_100.get_frame_ptr(), &frame, sizeof(frame));
+                else if (frame.can_id = 300)
+                    memcpy(data_300.get_frame_ptr(), &frame, sizeof(frame));
+                //else
+                //do nothing
             }
 
             if (data_100.get_mode() == SimulationMode::OFF)
@@ -81,14 +81,14 @@ int main()
                     //cf.CalculateFuel(data_100, data_200, data_300);
                     //ecm.UpdateECM(data_100.get_accelerator(), data_100.get_startstop(), data_200.get_driverinfo());
                     ecm.Update(data_100,data_300,1);
-
+                    
                 }
 
                 {
                     std::lock_guard<std::mutex> guard_write(data_200.fr200_mutex); // Onödigt...?
                     ecm.Write(data_200);
                     //data_200.set_fuelavg(cf.getFuelAvg());
-                    //data_200.set_fuelinst(cf.getFuelInst());                    
+                    //data_200.set_fuelinst(cf.getFuelInst());
                     //data_200.set_rpm(ecm.GetRPM());  // move somewhere else...
                 }
             }
