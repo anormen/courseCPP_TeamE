931ce3c4395a9830a559c45f64c4e79ca4ea83a1
jsuld <jakobsuld42@gmail.com>
2020-11-19 08:40:37 +0100

    restore old changes

--- src/TCM/gearbox.cpp
+++ src/TCM/gearbox.cpp
@@ -1,70 +1,87 @@
 #include "gearbox.hpp"
 
- Gearbox::Gearbox(){
+Gearbox::Gearbox()
+{
 
-        currentGear = 1;
-        targetGear = 1;
-        
-    }
+    currentGear = 1;
+    targetGear = 1;
+}
 
-// VehicleSpeed (km/h) = egRPM ( round per minut)* gearRatio (total with scaling 1000) * 
+// VehicleSpeed (km/h) = egRPM ( round per minut)* gearRatio (total with scaling 1000) *
 //wheelRadiusCircumferance in m 60 for rpm
 int Gearbox::getVehicleSpeed(const int _accPed, const int _engRpm)
 {
     int vehicleSpeed;
 
-    if ((_engRpm <900)&& (_accPed ==0)) 
+    std::cout << vehicleSpeed << "  " << lastVehicleSpeed << " " << _engRpm << std::endl;
+
+    if ((_engRpm <= 900) && (_accPed == 0))
     {
-        vehicleSpeed = 0;
+        if (this->lastVehicleSpeed > 0)
+            this->lastVehicleSpeed--;
+
+        vehicleSpeed = this->lastVehicleSpeed;
     }
-    else {
-        vehicleSpeed = (_engRpm * getGearRatio(_engRpm) * wheelCircumference)*6/(10000);
+    //else if (geae)  PARK and NEUTTRALm
+    else
+    {
+        vehicleSpeed = (_engRpm * getGearRatio(_engRpm) * wheelCircumference) * 6 / (10000);
+        lastVehicleSpeed = vehicleSpeed;
     }
-    return ( vehicleSpeed);
+    return (vehicleSpeed);
 }
-// GearRation 
-int Gearbox::getGearRatio (int _engRpm)
-{  
-    return ( gearRatioFact[targetGear]);
+// GearRation
+int Gearbox::getGearRatio(int _engRpm)
+{
+    return (gearRatioFact[targetGear]);
 }
 
-int Gearbox::getGear ()
-{  
+int Gearbox::getGear()
+{
     return (currentGear);
 }
 
 void Gearbox::selectGear(const int accPedal, const int _engRpm) {
     int accPedIndex = int (accPedal/10);
+    
+    if (multiShiftDelay < 20){
+        multiShiftDelay++;
+    }       
+    else {   
+        if (currentGear == 1){
+            if ((_engRpm > gearMapUpShift12[accPedIndex]))
+                targetGear = 2;}
+        else if(currentGear == 2)
+        {
+            if (_engRpm < ((double)gearMapUpShift12[accPedIndex]* ((double)gearRatioFact[1]/(double)gearRatioFact[2])))
+                targetGear = 1;
+            else if (_engRpm > gearMapUpShift23[accPedIndex])
+                targetGear = 3;
+        }
+        else if (currentGear == 3 ){
+            if (_engRpm < ((double)gearMapUpShift23[accPedIndex]* ((double)gearRatioFact[2]/(double)gearRatioFact[3])))
+                targetGear = 2;
+            else if (_engRpm > gearMapUpShift34[accPedIndex])
+                targetGear = 4;
+        } 
+            else if(currentGear == 4 ){
+            if (_engRpm < ((double)gearMapUpShift34[accPedIndex]* ((double)gearRatioFact[3]/(double)gearRatioFact[4])))
+                targetGear = 3;
+            else if (_engRpm > gearMapUpShift45[accPedIndex])
+                targetGear = 5;
+        }
+        else  /* currentGear == 5*/
+            if (_engRpm < ((double)gearMapUpShift45[accPedIndex]* ((double)gearRatioFact[4]/(double)gearRatioFact[5])))
+                targetGear = 4;
 
-    if (currentGear == 1){
-        if ((_engRpm > gearMapUpShift12[accPedIndex]))
-            targetGear = 2;}
-    else if(currentGear == 2)
-    {
-        if (_engRpm < (gearMapUpShift12[accPedIndex] - gearChangeHyst))
-            targetGear = 1;
-        else if (_engRpm > gearMapUpShift23[accPedIndex])
-            targetGear = 3;
+    if (targetGear != currentGear)
+        multiShiftDelay = 0;      
+    
     }
-    else if (currentGear == 3 ){
-        if (_engRpm < (gearMapUpShift23[accPedIndex] - gearChangeHyst))
-            targetGear = 2;
-        else if (_engRpm > gearMapUpShift34[accPedIndex] < _engRpm)
-            targetGear = 4;
-    } 
-        else if(currentGear == 4 ){
-        if (_engRpm < (gearMapUpShift34[accPedIndex] - gearChangeHyst))
-            targetGear = 3;
-        else if (_engRpm > gearMapUpShift45[accPedIndex])
-            targetGear = 5;
-    }
-    else  /* currentGear == 5*/
-        if (_engRpm < (gearMapUpShift45[accPedIndex] - gearChangeHyst))
-            targetGear = 4;
+    std::cout << multiShiftDelay <<  "multi" << targetGear << std::endl;       
     
     std::cout << currentGear << " " << targetGear  << "print in selectGear\n\r";
     currentGear = targetGear;
 }
-
 
 Gearbox::~Gearbox(){};
--- src/TCM/gearbox.hpp
+++ src/TCM/gearbox.hpp
@@ -5,19 +5,29 @@
 
 class Gearbox {
 private:
-    const int gearRatioFact[6] = {11, 10, 16, 22, 30, 40}; // R=0, 1,2 3,4,5
+    //const int gearRatioFact[6] = {11, 10, 14, 18, 28, 35}; // R=0, 1,2 3,4,5
+    const int gearRatioFact[6] = {11, 10, 11, 12, 13, 14};
     const int wheelCircumference = 2;// 2m 1913 mmmm for 17 tums
     
     // Select gear based on egRpm and AccPedal
     // Accped                            0,     10,   20,   30,   40,   50,   60,   70,   80,   90,  100
-    const int gearMapUpShift12[11] =   {1800, 2100, 2500, 3500, 3000, 3500, 3500, 3500, 3500, 4500, 5000 }; //rpm
-    const int gearMapUpShift23[11] =   {1800, 2100, 3000, 3500, 4000, 4500, 4500, 4500, 4500, 4500, 5000 }; //rpm
-    const int gearMapUpShift34[11] =   {1800, 2100, 3000, 3500, 4000, 4500, 4500, 4500, 4500, 4500, 5500 }; //rpm
-    const int gearMapUpShift45[11] =   {1800, 2100, 3000, 3500, 4000, 4500, 4500, 4500, 4500, 4500, 5500 }; //rpm    
+    //const int gearMapUpShift12[11] =   {1800, 2100, 2500, 3500, 3500, 3500, 3500, 3500, 3500, 4500, 5000 }; //rpm
+    //const int gearMapUpShift23[11] =   {1800, 2100, 3000, 3500, 3600, 4000, 4000, 4000, 4000, 4500, 5000 }; //rpm
+    //const int gearMapUpShift34[11] =   {1800, 2100, 3000, 3500, 3700, 4500, 4500, 4500, 4500, 4500, 5500 }; //rpm
+    //const int gearMapUpShift45[11] =   {1800, 2100, 3000, 3500, 4000, 4500, 4500, 4500, 4500, 4500, 5500 }; //rpm    
+    //int gearChangeHyst = 100; // rpm
+    // Accped                            0,     10,   20,   30,   40,   50,   60,   70,   80,   90,  100
+    const int gearMapUpShift12[11] =   {1450, 2100, 2500, 3500, 3500, 3500, 3500, 3500, 3500, 4500, 5000 }; //rpm
+    const int gearMapUpShift23[11] =   {1800, 2600, 2900, 3600, 3600, 4000, 4000, 4000, 4000, 4500, 5000 }; //rpm
+    const int gearMapUpShift34[11] =   {1800, 3100, 3600, 3600, 4100, 4400, 4500, 4500, 4500, 4500, 5500 }; //rpm
+    const int gearMapUpShift45[11] =   {1800, 3100, 3600, 3600, 4100, 4600, 4500, 4500, 4500, 4500, 5500 }; //rpm    
     int gearChangeHyst = 100; // rpm
+
 
     int currentGear;
     int targetGear;
+    int lastVehicleSpeed = 0;
+    int multiShiftDelay = 100;
 
 public:
     Gearbox();
