df2df25590abd2acb2817df72f941876e19d59a9
jsuld <jakobsuld42@gmail.com>
2020-11-19 14:37:18 +0100

    clean up

--- src/ECM/calcRPM.cpp
+++ src/ECM/calcRPM.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
 #include "calcRPM.hpp"
-//#include "frames.hpp"
 
 calcRPM::calcRPM()
 {
-    this->acc_inc_delta = 0;
-    this->target_rpm = 0;
     this->oldGearratio = 10;
-    /*
-    //this->RPM=900;
-    this->eng_on = false;
-    this->acc_inc_delta = 0;
-    this->acc_ped_stored = 0;
-    this->gear = 0;
-    this->target_rpm = 0; */
 }
 
 uint16_t calcRPM::CalculateRPM(int acc_ped, int _gearRatio, bool eng_on)
 {
-
-    std::cout << "_gearRatio = " << _gearRatio << "\n";
+    int target_rpm = 0;
+    _gearRatio = std::max(10, _gearRatio); // protect from zero
 
     // No magic values move to header
     if (acc_ped == 0)
@@ -42,73 +32,34 @@ uint16_t calcRPM::CalculateRPM(int acc_ped, int _gearRatio, bool eng_on)
         target_rpm = 6000;
     else if (acc_ped == 90)
         target_rpm = 6500;
-    else if (acc_ped == 45)
+    else if (acc_ped == 100)
         target_rpm = 7000;
-
-    std::cout << "_gearRatio = " << _gearRatio << " oldGearratio = " <<  oldGearratio << "\n";
 
     if (oldGearratio != _gearRatio)
     {
 
-        if (_gearRatio >= 10) //protect from 0
-        {
-            this->rpm = (double)this->rpm * ((double)this->oldGearratio / (double)_gearRatio);
-            //this->rpm -=100;
-            this->oldGearratio = _gearRatio;
-        }
+        this->rpm = (double)this->rpm * ((double)this->oldGearratio / (double)_gearRatio);
+        this->oldGearratio = _gearRatio;
     }
 
-    std::cout << "RPM = " << this->rpm << " target_rpm = " << target_rpm << std::endl;
-    std::cout << "acc_ped = " << acc_ped << " acc_ped_stored = " << acc_ped_stored << std::endl;
- 
-    increasing_rpm = target_rpm > this->rpm;
-    decreasing_rpm = target_rpm < this->rpm;
+    bool increasing_rpm = target_rpm > this->rpm;
+    bool decreasing_rpm = target_rpm < this->rpm;
 
-    std::cout << "increasing_rpm = " << increasing_rpm << std::endl;
-    std::cout << "decreasing_rpm = " << decreasing_rpm << std::endl;
-    std::cout << "eng_on = " << eng_on << std::endl;
     if (eng_on == true)
     {
         if (this->rpm < 900)
             this->rpm = 900;
 
         if (increasing_rpm)
-        {
-            this->acc_inc_delta += 100; // not hard code, use some update rate
-            std::cout << "acc_inc_delta = " << acc_inc_delta << std::endl;
-
-            if (this->rpm >= target_rpm) // || this->RPM >= max_rpm)
-            {
-                increasing_rpm = false;
-                acc_inc_delta = 0;
-            }
             this->rpm += acc_ped * 10 / _gearRatio;
-            //if (acc_inc_delta > response_time)
-            //    this->rpm += 10;
-                //this->rpm += 20 * acc_ped / 20;
-        }
 
         if (decreasing_rpm)
-        {
-            this->rpm -= 60; // how should we calculate decrease rate?
-            if (this->rpm < 900)
-            {
-                this->rpm = 900;
-                decreasing_rpm = false;
-            }
-            if (this->rpm < target_rpm)
-            {
-                //this->RPM = target_rpm;
-                decreasing_rpm = false;
-            }
-        }
+            this->rpm -= 60;
     }
     else
     {
         this->rpm = 0;
     }
-    acc_ped_stored = acc_ped;
-    std::cout << "\n\n";
 
     return this->rpm;
 }
--- src/ECM/calcRPM.hpp
+++ src/ECM/calcRPM.hpp
@@ -2,7 +2,6 @@
 #define CLACRPM_HPP
 
 #include<iostream>
-//#include "frames.hpp"
 
 class calcRPM {
   public:
@@ -12,13 +11,6 @@ class calcRPM {
     uint16_t rpm;
     int oldGearratio = 10;
     bool eng_on;
-    const int response_time=1000; // ms
-    int acc_inc_delta;
-    int acc_ped_stored;
-    bool increasing_rpm;
-    bool decreasing_rpm;
-    int gear; // delete later
-    int target_rpm;
 };
 
 #endif // CALCRPM_HPP
