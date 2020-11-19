049365601dad5a1b143cf33377ac68c70e0fed1d
Mikko Härmä <mikko.harma@volvocars.com>
2020-11-19 09:00:41 +0100

    Updated reading

--- src/TCM/main.cpp
+++ src/TCM/main.cpp
@@ -15,7 +15,6 @@ int main()
     frame_100 data_100;
     frame_200 data_200;
     frame_300 data_300;    
-    uint8_t sendRate = 0;
 
     TCM tcm;
 
@@ -24,36 +23,33 @@ int main()
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(fr100_updateRate));
             std::cout << "Read frame" << std::endl;
-            can.canReadFrame(frame);
-            can.printFrame(frame);
+            while(can.canReadFrame(frame) > 0){
+                can.printFrame(frame);
             
-            if (frame.can_dlc > 0 && frame.can_id != 0)
-            {
-                std::lock_guard<std::mutex> guard(data_100.fr100_mutex);
-                if (frame.can_id == 100)
-                    memcpy(data_100.get_frame_ptr(), &frame, sizeof(frame));                        
-                else if (frame.can_id == 200)
-                    memcpy(data_200.get_frame_ptr(), &frame, sizeof(frame));
-                //else
-                    //do nothing
+                if (frame.can_dlc > 0 && frame.can_id != 0)
+                {
+                    std::lock_guard<std::mutex> guard(data_100.fr100_mutex);
+                    if (frame.can_id == 100)
+                        memcpy(data_100.get_frame_ptr(), &frame, sizeof(frame));                        
+                    else if (frame.can_id == 200)
+                        memcpy(data_200.get_frame_ptr(), &frame, sizeof(frame));
+                    //else
+                        //do nothing
+                }   
             }
-
             if (data_100.get_mode() == SimulationMode::OFF)
             {
                 std::cout << "Exit IO thread" << std::endl;
                 break;
             }
-            if(sendRate > 4)
+
             {
                 std::cout << "Write frame" << std::endl;
                 std::lock_guard<std::mutex> guard(data_300.fr300_mutex);
                 memcpy(&frame, data_300.get_frame_ptr(), sizeof(frame));
                 can.canWriteFrame(frame);
                 can.printFrame(frame);
-                sendRate = 0;
             }
-            else
-                sendRate++;
 
             std::cout << "\033c \033[0;32m" ; // clear screen
         }
